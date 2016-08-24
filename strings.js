6c72f5393436b5e9b2323ba9753d1da0b4d1bc6a
Lindsey Stickline <lindseystickline@Lindseys-MacBook-Air.local>
2016-08-23 21:28:48 -0500

    Completed exercise; need to add clear function to submit

--- strings.js
+++ strings.js
@@ -1,1 +1,38 @@
-console.log("Test")
+var testString = "";
+
+// To make the input field accept LETTERS ONLY:
+function alphaOnly(event) {
+  var key = event.keyCode;
+  return ((key >= 65 && key <= 90) || key == 8); // key == 8 allows backspace
+};
+
+// Submit Button once clicked:
+var submit = document.getElementById("submit");
+submit.addEventListener("click", submitInput);
+
+function submitInput() {
+	testString = document.getElementById("userinput").value;
+	reversal();
+	alphabits();
+	palindrome();
+}
+
+// Execute these functions on submit:
+function reversal() {
+	var wordReverse = testString.split('').reverse().join('');
+	document.getElementById("backwardsoutput").innerHTML += wordReverse;
+}
+
+function alphabits() {
+	var wordAlphabetize = testString.split('').sort().join('');
+	document.getElementById("alphabeticaloutput").innerHTML += wordAlphabetize;
+}
+
+function palindrome() {
+	if (testString == testString.split('').reverse().join('')) {
+		document.getElementById("palindromeoutput").innerHTML += "Your word is a palindrome!";	
+	}
+	else {
+		document.getElementById("palindromeoutput").innerHTML += "Your word is NOT a palindrome!";
+	}
+}
